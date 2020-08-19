b1a9b1fc90e3858f3f660c1e3c3895457935b5ba
Shahid N. Shah <shahid@shah.org>
2020-08-18 20:41:19 -0400

    Use new *.gim.ts dependencies

--- rds/diagram/deps-test.ts
+++ rds/diagram/deps-test.ts
@@ -1,4 +1,4 @@
 // Dependencies required during engineering/testing only (not in production)
 export * as testingAsserts from "https://deno.land/std@v0.65.0/testing/asserts.ts";
-export * as governedImTestModel from "https://denopkg.com/gov-suite/governed-im-rds@v0.5.0/models/test-model.im.ts";
+export * as governedImTestModel from "https://denopkg.com/gov-suite/governed-im-rds@v0.6.0/models/test-model.gim.ts";
 export * as specModule from "https://denopkg.com/shah/specification-module@v1.0.4/mod.ts";
--- rds/sql/deps-test.ts
+++ rds/sql/deps-test.ts
@@ -1,3 +1,3 @@
 // Dependencies required during engineering/testing only (not in production)
 export * as testingAsserts from "https://deno.land/std@v0.65.0/testing/asserts.ts";
-export * as governedImTestModel from "https://denopkg.com/gov-suite/governed-im-rds@v0.5.0/models/test-model.im.ts";
+export * as governedImTestModel from "https://denopkg.com/gov-suite/governed-im-rds@v0.6.0/models/test-model.gim.ts";
