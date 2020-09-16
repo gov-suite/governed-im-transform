834ad4ec7418647202597446d8e113e220ae2ca7
Shahid N. Shah <shahid@shah.org>
2020-09-16 19:47:48 -0400

    Update dependencies

--- rds/sql/deps.ts
+++ rds/sql/deps.ts
@@ -1,7 +1,7 @@
-export * as governedIM from "https://denopkg.com/gov-suite/governed-im@v0.6.1/core/mod.ts";
-export * as governedImRDS from "https://denopkg.com/gov-suite/governed-im-rds@v0.10.2/rds/mod.ts";
-export * as governedImRDSModels from "https://denopkg.com/gov-suite/governed-im-rds@v0.10.2/models/mod.ts";
-export * as governedImTransform from "https://denopkg.com/gov-suite/governed-im@v0.6.1/transform/mod.ts";
+export * as governedIM from "https://denopkg.com/gov-suite/governed-im@v0.6.4/core/mod.ts";
+export * as governedImRDS from "https://denopkg.com/gov-suite/governed-im-rds@v0.10.4/rds/mod.ts";
+export * as governedImRDSModels from "https://denopkg.com/gov-suite/governed-im-rds@v0.10.4/models/mod.ts";
+export * as governedImTransform from "https://denopkg.com/gov-suite/governed-im@v0.6.4/transform/mod.ts";
 export * as artfPersist from "https://denopkg.com/shah/artifacts-persistence@v1.0.21/mod.ts";
 export * as polyglotArtfNature from "https://denopkg.com/shah/artifacts-persistence@v1.0.21/polyglot/mod.ts";
 export * as contextMgr from "https://denopkg.com/shah/context-manager@v1.0.6/mod.ts";
