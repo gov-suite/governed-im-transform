fffdcf3909d86b089f95d8b3a7085b462a07e55e
divya-sk <divya.kumar@citrusinformatics.com>
2020-11-19 10:52:21 +0530

    Update  version changes

--- rds/diagram/deps-test.ts
+++ rds/diagram/deps-test.ts
@@ -1,4 +1,4 @@
 // Dependencies required during engineering/testing only (not in production)
-export * as testingAsserts from "https://deno.land/std@v0.70.0/testing/asserts.ts";
-export * as governedImTestModel from "https://denopkg.com/gov-suite/governed-im-rds@v0.10.5/models/test-model.gim.ts";
+export * as testingAsserts from "https://deno.land/std@0.78.0/testing/asserts.ts";
+export * as governedImTestModel from "https://denopkg.com/gov-suite/governed-im-rds@v0.10.6/models/test-model.gim.ts";
 export * as specModule from "https://denopkg.com/shah/specification-module@v1.0.6/mod.ts";
--- rds/diagram/deps.ts
+++ rds/diagram/deps.ts
@@ -1,6 +1,6 @@
-export * as governedImRDS from "https://denopkg.com/gov-suite/governed-im-rds@v0.10.5/rds/mod.ts";
-export * as governedIM from "https://denopkg.com/gov-suite/governed-im@v0.6.5/core/mod.ts";
-export * as governedImTransform from "https://denopkg.com/gov-suite/governed-im@v0.6.5/transform/mod.ts";
-export * as artfPersist from "https://denopkg.com/shah/artifacts-persistence@v1.0.24/mod.ts";
-export * as polyglotArtfNature from "https://denopkg.com/shah/artifacts-persistence@v1.0.24/polyglot/mod.ts";
-export * as textInflect from "https://denopkg.com/shah/text-inflect@v1.0.4/mod.ts";
+export * as governedImRDS from "https://denopkg.com/gov-suite/governed-im-rds@v0.10.6/rds/mod.ts";
+export * as governedIM from "https://denopkg.com/gov-suite/governed-im@v0.6.6/core/mod.ts";
+export * as governedImTransform from "https://denopkg.com/gov-suite/governed-im@v0.6.6/transform/mod.ts";
+export * as artfPersist from "https://denopkg.com/shah/artifacts-persistence@v1.1.0/mod.ts";
+export * as polyglotArtfNature from "https://denopkg.com/shah/artifacts-persistence@v1.1.0/polyglot/mod.ts";
+export * as textInflect from "https://denopkg.com/shah/text-inflect@v1.0.5/mod.ts";
