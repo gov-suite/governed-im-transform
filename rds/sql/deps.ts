eab9ab60231555e902fa0d75d5adf266f5d291b6
Shahid N. Shah <shahid@shah.org>
2020-12-15 18:00:21 -0500

    fix: revert Deno 1.6 upgrade bug

--- rds/sql/deps.ts
+++ rds/sql/deps.ts
@@ -1,6 +1,6 @@
 export * as governedIM from "https://denopkg.com/gov-suite/governed-im@v0.7.0/core/mod.ts";
-export * as governedImRDS from "https://denopkg.com/gov-suite/governed-im-rds@v0.10.7/rds/mod.ts";
-export * as governedImRDSModels from "https://denopkg.com/gov-suite/governed-im-rds@v0.10.7/models/mod.ts";
+export * as governedImRDS from "https://denopkg.com/gov-suite/governed-im-rds@v0.10.8/rds/mod.ts";
+export * as governedImRDSModels from "https://denopkg.com/gov-suite/governed-im-rds@v0.10.8/models/mod.ts";
 export * as governedImTransform from "https://denopkg.com/gov-suite/governed-im@v0.7.0/transform/mod.ts";
 export * as artfPersist from "https://denopkg.com/shah/artifacts-persistence@v1.1.1/mod.ts";
 export * as polyglotArtfNature from "https://denopkg.com/shah/artifacts-persistence@v1.1.1/polyglot/mod.ts";
