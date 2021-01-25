8810256b5521aec79807324ea2d4fe46755fc07d
Shahid N. Shah <shahid@shah.org>
2021-01-25 09:43:41 -0500

    fix: update dependencies

--- rds/sql/deps.ts
+++ rds/sql/deps.ts
@@ -1,9 +1,9 @@
-export * as governedIM from "https://denopkg.com/gov-suite/governed-im@v0.7.0/core/mod.ts";
-export * as governedImRDS from "https://denopkg.com/gov-suite/governed-im-rds@v0.10.8/rds/mod.ts";
-export * as governedImRDSModels from "https://denopkg.com/gov-suite/governed-im-rds@v0.10.8/models/mod.ts";
-export * as governedImTransform from "https://denopkg.com/gov-suite/governed-im@v0.7.0/transform/mod.ts";
-export * as artfPersist from "https://denopkg.com/shah/artifacts-persistence@v1.1.1/mod.ts";
-export * as polyglotArtfNature from "https://denopkg.com/shah/artifacts-persistence@v1.1.1/polyglot/mod.ts";
+export * as governedIM from "https://denopkg.com/gov-suite/governed-im@v0.7.1/core/mod.ts";
+export * as governedImRDS from "https://denopkg.com/gov-suite/governed-im-rds@v0.10.9/rds/mod.ts";
+export * as governedImRDSModels from "https://denopkg.com/gov-suite/governed-im-rds@v0.10.9/models/mod.ts";
+export * as governedImTransform from "https://denopkg.com/gov-suite/governed-im@v0.7.1/transform/mod.ts";
+export * as artfPersist from "https://denopkg.com/shah/artifacts-persistence@v1.2.0/mod.ts";
+export * as polyglotArtfNature from "https://denopkg.com/shah/artifacts-persistence@v1.2.0/polyglot/mod.ts";
 export * as contextMgr from "https://denopkg.com/shah/context-manager@v1.0.7/mod.ts";
 export * as specModule from "https://denopkg.com/shah/specification-module@v1.0.9/mod.ts";
 export * as textInflect from "https://denopkg.com/shah/text-inflect@v1.0.5/mod.ts";
