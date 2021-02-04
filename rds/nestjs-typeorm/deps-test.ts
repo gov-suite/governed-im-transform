c018ce81aba027134d65577a90799ba9ea8c55f7
Shahid N. Shah <shahid@shah.org>
2021-02-03 19:26:48 -0500

    fix: update latest dependencies

--- rds/nestjs-typeorm/deps-test.ts
+++ rds/nestjs-typeorm/deps-test.ts
@@ -1,4 +1,4 @@
 // Dependencies required during engineering/testing only (not in production)
-export * as testingAsserts from "https://deno.land/std@0.84.0/testing/asserts.ts";
-export * as governedImTestModel from "https://denopkg.com/gov-suite/governed-im-rds@v0.10.9/models/test-model.gim.ts";
+export * as testingAsserts from "https://deno.land/std@0.85.0/testing/asserts.ts";
+export * as governedImTestModel from "https://denopkg.com/gov-suite/governed-im-rds@v0.10.10/models/test-model.gim.ts";
 export * as specModule from "https://denopkg.com/shah/specification-module@v1.0.9/mod.ts";
--- rds/sql/deps.ts
+++ rds/sql/deps.ts
@@ -1,7 +1,7 @@
-export * as governedIM from "https://denopkg.com/gov-suite/governed-im@v0.7.1/core/mod.ts";
-export * as governedImRDS from "https://denopkg.com/gov-suite/governed-im-rds@v0.10.9/rds/mod.ts";
-export * as governedImRDSModels from "https://denopkg.com/gov-suite/governed-im-rds@v0.10.9/models/mod.ts";
-export * as governedImTransform from "https://denopkg.com/gov-suite/governed-im@v0.7.1/transform/mod.ts";
+export * as governedIM from "https://denopkg.com/gov-suite/governed-im@v0.7.2/core/mod.ts";
+export * as governedImRDS from "https://denopkg.com/gov-suite/governed-im-rds@v0.10.10/rds/mod.ts";
+export * as governedImRDSModels from "https://denopkg.com/gov-suite/governed-im-rds@v0.10.10/models/mod.ts";
+export * as governedImTransform from "https://denopkg.com/gov-suite/governed-im@v0.7.2/transform/mod.ts";
 export * as artfPersist from "https://denopkg.com/shah/artifacts-persistence@v1.2.0/mod.ts";
 export * as polyglotArtfNature from "https://denopkg.com/shah/artifacts-persistence@v1.2.0/polyglot/mod.ts";
 export * as contextMgr from "https://denopkg.com/shah/context-manager@v1.0.7/mod.ts";
