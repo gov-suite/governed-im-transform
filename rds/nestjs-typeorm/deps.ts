71305ee2c3a06bac04005e3bd01e0e8b34492038
SumiSujith <sumi.mathew@citrusinformatics.com>
2021-04-28 17:16:49 +0530

    Update latest dependency

--- rds/nestjs-typeorm/deps.ts
+++ rds/nestjs-typeorm/deps.ts
@@ -1,4 +1,4 @@
-export * as governedImRDS from "https://denopkg.com/gov-suite/governed-im-rds@v0.10.14/rds/mod.ts";
+export * as governedImRDS from "https://denopkg.com/gov-suite/governed-im-rds@v0.10.15/rds/mod.ts";
 export * as governedIM from "https://denopkg.com/gov-suite/governed-im@v0.7.4/core/mod.ts";
 export * as governedImTransform from "https://denopkg.com/gov-suite/governed-im@v0.7.4/transform/mod.ts";
 export * as artfPersist from "https://denopkg.com/shah/artifacts-persistence@v1.2.0/mod.ts";
