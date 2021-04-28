79d8edc7e7c9afc85ca603a99bce63e5d6ae8a7e
SumiSujith <sumi.mathew@citrusinformatics.com>
2021-04-28 15:08:20 +0530

    Change procedure return type to UUID

--- rds/diagram/deps.ts
+++ rds/diagram/deps.ts
@@ -1,6 +1,9 @@
-export * as governedImRDS from "https://denopkg.com/gov-suite/governed-im-rds@v0.10.13/rds/mod.ts";
-export * as governedIM from "https://denopkg.com/gov-suite/governed-im@v0.7.4/core/mod.ts";
-export * as governedImTransform from "https://denopkg.com/gov-suite/governed-im@v0.7.4/transform/mod.ts";
+//export * as governedImRDS from "https://denopkg.com/gov-suite/governed-im-rds@v0.10.13/rds/mod.ts";
+//export * as governedIM from "https://denopkg.com/gov-suite/governed-im@v0.7.4/core/mod.ts";
+//export * as governedImTransform from "https://denopkg.com/gov-suite/governed-im@v0.7.4/transform/mod.ts";
+export * as governedImRDS from "../../../governed-im-rds/rds/mod.ts";
+export * as governedIM from "../../../governed-im/core/mod.ts";
+export * as governedImTransform from "../../../governed-im/transform/mod.ts";
 export * as artfPersist from "https://denopkg.com/shah/artifacts-persistence@v1.2.0/mod.ts";
 export * as polyglotArtfNature from "https://denopkg.com/shah/artifacts-persistence@v1.2.0/polyglot/mod.ts";
 export * as textInflect from "https://denopkg.com/shah/text-inflect@v1.0.5/mod.ts";
